7cfe468264a7623b2bda2fcf10784289da550ee4
Pablo Filetti Moreira <pablo.filetti@gmail.com>
2013-03-16 19:57:03 +0000

    Refatoração

--- src/main/java/br/com/atos/gc/componente/GridXhtmlColunasComponente.java
+++ src/main/java/br/com/atos/gc/componente/GridXhtmlColunasComponente.java
@@ -30,12 +30,14 @@ public class GridXhtmlColunasComponente extends Componente {
 				
 				if (AtributoManyToOne.class.isInstance(atributo)) {
 					AtributoManyToOne atributoManyToOne =  (AtributoManyToOne) atributo;
-					println(pw, "\t\t\t<p:column headerText=\"{0}\" sortBy=\"#'{'objeto.{1}.{2}'}'\">", atributo.getRotulo(), atributo.getField().getName(), atributoManyToOne.getAssociacaoAtributoDescricao());
+					println(pw, "\t\t\t<p:column sortBy=\"#'{'objeto.{1}.{2}'}'\">", atributo.getRotulo(), atributo.getField().getName(), atributoManyToOne.getAssociacaoAtributoDescricao());
+					println(pw, "\t\t\t\t<f:facet name=\"header\"><h:outputText value=\"{0}\" /></f:facet>", atributo.getRotulo());
 					printOutputText(pw, "\t\t\t\t", atributoManyToOne.getAssociacaoAtributoField(), path + atributo.getField().getName() + "." + atributoManyToOne.getAssociacaoAtributoDescricao());
 					println(pw, "\t\t\t</p:column>");
 				}
 				else {
-					println(pw, "\t\t\t<p:column headerText=\"{0}\" sortBy=\"#'{'objeto.{1}'}'\">", atributo.getRotulo(), atributo.getField().getName());
+					println(pw, "\t\t\t<p:column sortBy=\"#'{'objeto.{1}'}'\">", atributo.getRotulo(), atributo.getField().getName());
+					println(pw, "\t\t\t\t<f:facet name=\"header\"><h:outputText value=\"{0}\" /></f:facet>", atributo.getRotulo());
 					printOutputText(pw, "\t\t\t\t", atributo.getField(), path + atributo.getField().getName());
 					println(pw, "\t\t\t</p:column>");
 				}
