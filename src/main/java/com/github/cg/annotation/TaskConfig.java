1253926fad62fa72896045d51f9b62954a838107
Pablo Filetti Moreira <pablo.filetti@gmail.com>
2015-08-07 11:31:10 -0300

    Refatoração da anotação taskconfig para pegar valores em tempo de execução.

--- src/main/java/com/github/cg/annotation/TaskConfig.java
+++ src/main/java/com/github/cg/annotation/TaskConfig.java
@@ -1,5 +1,9 @@
 package com.github.cg.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface TaskConfig {
 
 	/**
